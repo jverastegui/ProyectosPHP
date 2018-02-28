defbd774f8b26995dd91042984c26448ddfaf277
Jeferson Verastegui Quispe <jveraste@everis.com>
2018-02-28 18:22:00 -0500

    g

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>BD22</title>
+    <title>BD88</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
 </head>
 <body>
-    sdsads
+    
 </body>
 </html>
