2fc31835a2fc7c9bd9845e0043de5e688b39808e
Jeferson Verastegui Quispe <jveraste@everis.com>
2018-03-01 14:32:35 -0500

    as

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>BD8800022221993ss</title>
+    <title>BD8800022221993ssoo</title>
     <meta name="viewports" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
